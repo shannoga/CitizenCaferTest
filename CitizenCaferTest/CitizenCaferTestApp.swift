fb9adf38994e050b5f800c06f89a8a2821bf1d38
Shani Hajbi <shannoga@me.com>
2026-08-03 14:22:05 +0300

    Initial Commit

---
+++ CitizenCaferTest/CitizenCaferTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitizenCaferTestApp.swift
+//  CitizenCaferTest
+//
+//  Created by Shani Hajbi on 03/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CitizenCaferTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
